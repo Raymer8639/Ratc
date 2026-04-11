3c5dd6796e8f6398438ffddb13d371e056110b0f
Raymer8639 <diliyangemail@gmail.com>
2026-04-11 22:59:22 +0800

    Added features

--- ravm/src/main.rs
+++ ravm/src/main.rs
@@ -1,3 +1,9 @@
+use clap::Parser;
+use ravm_args::Arg;
+
 fn main() {
-    println!("Hello, world!");
+    let arg = Arg::parse();
+    for file in arg.files {
+        ravm_reader::reader(file.clone());
+    }
 }
---
+++ ravm_reader/src/lib.rs
@@ -0,0 +1,1 @@
+pub fn reader(file: String) {}
